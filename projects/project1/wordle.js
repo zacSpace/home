99da029fe738387d4bd5e312a58a57dc8caa7a06
zacSpace <zwn2000@columbia.edu>
2025-08-27 20:26:00 -0400

    adding wordle basics

---
+++ projects/project1/wordle.js
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Zac's Wordle</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale =0, user-scalable=no">
+        <body>
+
+        </body>
+    </head>
+
+</html>
